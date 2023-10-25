0d2a7d0b72d388eacaf7adf6f54997f0855f4cb9
camiloDanielli <daniellicamilo6@gmail.com>
2023-10-24 21:40:53 -0300

    ejercicio1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using SixLabors.ImageSharp.Formats.Bmp;
 
 namespace CompAndDel
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // cargar una imagen
+            PictureProvider provider = new PictureProvider();
+            IPicture image = provider.GetPicture(@"..\..\..\beer.jpg");
+            IPipe pipeNull = new PipeNull();
+            IFilter Filto1 = new FilterNegative();
+            IPipe pipe2 = new PipeSerial(Filto1, pipeNull);
+            IFilter Filto2 = new FilterGreyscale();
+            IPipe pipe1 = new PipeSerial(Filto2, pipe2);
+
+            IPicture processedImage = pipe1.Send(image);
+
+
+            provider.SavePicture(processedImage, @"..\..\..\beer1.jpg");
         }
     }
 }
